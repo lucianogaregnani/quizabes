8d09e61b5db9f1e35526367fc706f6a215625918
lucianogaregnani <luciano.garegnani@estudiantes.unahur.edu.ar>
2024-01-11 14:29:49 -0300

    list of themes finish

--- src/pages/Themes/Themes.tsx
+++ src/pages/Themes/Themes.tsx
@@ -4,7 +4,7 @@ import themes from "./themes.module.scss"
 function Themes() {
   return (
     <section className={themes.themesSection}>
-      <h1>Choose the theme you like the most!</h1>
+      <h1>Choose <span>the theme</span> you like the most<span>!</span></h1>
       <ListOfThemes />
     </section>
   );
