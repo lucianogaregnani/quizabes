e9e54602bca67437e675d45a479d7c7501b56413
lucianogaregnani <luciano.garegnani@estudiantes.unahur.edu.ar>
2024-01-17 00:06:28 -0300

    error message

--- src/pages/Questions/Questions.tsx
+++ src/pages/Questions/Questions.tsx
@@ -1,15 +1,18 @@
 import questions from "./questions.module.scss";
 import Question from "./components/Question";
 import useQuestions from "./hooks/useQuestions";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { themes } from "../Themes/models";
 import { useAppSelector } from "./hooks/store";
+import ReactLoading from "react-loading";
 import Score from "./pages/Score/Score";
+import { Button } from "../../components";
+
 function Questions() {
   const { themeId } = useParams();
   const { loading, error } = useQuestions({ themeId });
   const itsGameFinish = useAppSelector((state) => state.gameFinish);
-  
+
   return (
     <section className={questions.questionsSection}>
       {itsGameFinish ? (
@@ -19,8 +22,22 @@ function Questions() {
           {!loading && !error && (
             <h1>{themes.find((theme) => theme.to === themeId)?.name}</h1>
           )}
-          {loading && <p>loading...</p>}
-          {error && <p>{error}</p>}
+          {loading && (
+            <ReactLoading
+              type="bars"
+              color="white"
+              height={"20%"}
+              width={"20%"}
+            />
+          )}
+          {error && (
+            <div className={questions.errorMessage}>
+              <p>Ups... {error}&#128517;</p>
+              <Link to="/themes">
+                <Button text="Return to themes" />
+              </Link>
+            </div>
+          )}
           {!loading && !error && <Question />}
         </>
       )}
