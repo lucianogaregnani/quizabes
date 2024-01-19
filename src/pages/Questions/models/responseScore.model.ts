445708136d6e1ab023bc5fc95322040dcdf45807
lucianogaregnani <luciano.garegnani@estudiantes.unahur.edu.ar>
2024-01-19 17:28:38 -0300

    fix

--- src/pages/Questions/models/responseScore.model.ts
+++ src/pages/Questions/models/responseScore.model.ts
@@ -2,32 +2,32 @@ export const responseScore = {
     easy: {
         boolean: {
             correct: 5,
-            incorrect: 10 
+            incorrect: -10 
         },
         multiple: {
             correct: 10,
-            incorrect: 20 
+            incorrect: -20 
         }
     },
     medium: {
         boolean: {
-            correct: 5,
-            incorrect: 10 
+            correct: 10,
+            incorrect: -10 
         },
         multiple: {
             correct: 15,
-            incorrect: 15
+            incorrect: -15
         }
 
     },
     hard: {
         boolean: {
-            correct: 5,
-            incorrect: 10 
+            correct: 15,
+            incorrect: -5 
         },
         multiple: {
             correct: 20,
-            incorrect: 10
+            incorrect: -10
         }
     }
 }
--- src/pages/Questions/utils/reformatText.util.ts
+++ src/pages/Questions/utils/reformatText.util.ts
@@ -4,7 +4,7 @@ export const reformatText = (text: string) => {
 
   if (text.includes("#039;")) newText = newText.replace(/#039;/g, "'");
 
-  if (text.includes("&'")) newText = newText.replace(/&'/g, '"');
+  if (text.includes("&'")) newText = newText.replace(/&'/g, "'");
 
   return newText;
 };
